857fc13217c2d7f4e9752f88bca2b38e649df5aa
demetriustw <56927431+demetriustw@users.noreply.github.com>
2022-12-14 15:00:39 -0700

    Add files via upload

--- makeATable.sql
+++ makeATable.sql
@@ -1467,3 +1467,203 @@ SELECT @@SESSION.sql_mode;
 SELECT title, rating FROM series
 JOIN reviews ON reviews.series_id = series.id
 GROUP BY title;
+
+-- SECTIONS 16-17
+
+-- WINDOWS FUNCTIONS
+-- OVER
+CREATE DATABASE window_funcs;
+
+CREATE TABLE employees (
+    emp_no INT PRIMARY KEY AUTO_INCREMENT,
+    department VARCHAR(20),
+    salary INT
+);
+ 
+INSERT INTO employees (department, salary) VALUES
+('engineering', 80000),
+('engineering', 69000),
+('engineering', 70000),
+('engineering', 103000),
+('engineering', 67000),
+('engineering', 89000),
+('engineering', 91000),
+('sales', 59000),
+('sales', 70000),
+('sales', 159000),
+('sales', 72000),
+('sales', 60000),
+('sales', 61000),
+('sales', 61000),
+('customer service', 38000),
+('customer service', 45000),
+('customer service', 61000),
+('customer service', 40000),
+('customer service', 31000),
+('customer service', 56000),
+('customer service', 55000);
+
+
+SELECT department, AVG(salary) FROM employees
+    GROUP BY department;
+
+SELECT AVG(salary) FROM employees;
+
+-- WINDOW FUNCTIONS: RUNS CODE WITH EACH ROW
+SELECT AVG(salary) OVER() FROM employees;
+
+SELECT emp_no, department, salary, AVG(salary) OVER() FROM employees;
+
+-- WORKS BECAUSE OVER
+SELECT 
+    emp_no, 
+    department, salary,
+    MIN(salary) OVER(),
+    MAX(salary) OVER() 
+FROM employees;
+
+-- WORKS AS ABOVE
+SELECT 
+    MIN(salary),
+    MAX(salary) 
+FROM employees;
+
+-- DON'T WORK BECAUSE GROUP BY
+SELECT 
+    emp_no, 
+    department, salary,
+    MIN(salary),
+    MAX(salary) 
+FROM employees;
+
+-- PARTITION BY
+SELECT emp_no, department, salary, AVG(salary) OVER(PARTITION BY department) FROM employees;
+
+SELECT 
+    emp_no, 
+    department, 
+    salary,
+    AVG(salary) OVER(PARTITION BY department) AS dept_avg
+FROM employees;
+
+SELECT department, AVG(salary) FROM employees GROUP BY department;
+
+SELECT 
+    emp_no, 
+    department, 
+    salary,
+    AVG(salary) OVER(PARTITION BY department) AS dept_avg,
+    AVG(salary) OVER() AS company_avg
+FROM employees;
+
+SELECT emp_no, department, salary, COUNT(*) OVER(PARTITION BY department) AS dept_count
+FROM employees;
+
+SELECT 
+    emp_no, 
+    department, 
+    salary,
+    SUM(salary) OVER(PARTITION BY department) AS dept_payroll,
+    SUM(salary) OVER() AS total_payroll
+FROM employees;
+
+-- ORDER BY (WITH OVER)
+-- DIVIDES UP ALL THE ROWS
+SELECT 
+    emp_no, 
+    department, 
+    salary,
+    SUM(salary) OVER(PARTITION BY department ORDER BY salary) AS rolling_dept_salary,
+    SUM(salary) OVER(PARTITION BY department) AS total_dept_salary
+FROM employees;
+
+SELECT 
+    emp_no, 
+    department, 
+    salary,
+    MIN(salary) OVER(PARTITION BY department ORDER BY salary DESC) as rolling_min
+FROM employees;
+
+SELECT 
+    emp_no, 
+    department, 
+    salary,
+    RANK() OVER(PARTITION BY department ORDER BY salary DESC) AS dept_salary_rank,
+    RANK() OVER(ORDER BY salary DESC) AS overall_salary_rank
+FROM employees ORDER BY department;
+
+-- DENSE_RANK, & ROW_NUMBER()
+SELECT 
+    emp_no, 
+    department, 
+    salary,
+    ROW_NUMBER() OVER(PARTITION BY department ORDER BY salary DESC) AS dept_row_number,
+    RANK() OVER(PARTITION BY department ORDER BY salary DESC) AS dept_salary_rank,
+    RANK() OVER(ORDER BY salary DESC) AS overall_rank,
+    DENSE_RANK() OVER(ORDER BY salary DESC) AS overall_dense_rank,
+    ROW_NUMBER() OVER(ORDER BY salary DESC) AS overall_num
+FROM employees ORDER BY overall_rank;
+
+-- NTILE(N)
+SELECT 
+    emp_no, 
+    department, 
+    salary,
+    NTILE(4) OVER(PARTITION BY department ORDER BY salary DESC) AS dept_salary_quartile,
+    NTILE(4) OVER(ORDER BY salary DESC) AS salary_quartile
+FROM employees;
+
+-- FIRST_VALUE
+SELECT 
+    emp_no, 
+    department, 
+    salary,
+    FIRST_VALUE(emp_no) OVER(PARTITION BY department ORDER BY salary DESC) AS highest_paid_dept,
+    FIRST_VALUE(emp_no) OVER(ORDER BY salary DESC) AS highest_paid_overall
+FROM employees;
+
+-- LEAD AND LAG
+SELECT 
+    emp_no, 
+    department, 
+    salary,
+    salary - LEAD(salary) OVER(ORDER BY salary DESC) AS salary_diff
+FROM employees;
+
+SELECT 
+    emp_no, 
+    department, 
+    salary,
+    salary - LAG(salary) OVER(PARTITION BY department ORDER BY salary DESC) AS dept_salary_diff
+FROM employees;
+
+-- SCHEMA DESIGN
+CREATE DATABASE ig_clone;
+USE ig_clone;
+
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+)
+
+CREATE TABLE likes (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    FOREIGN KEY (username) REFERENCES users(username)
+)
+
+CREATE TABLE photos (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    FOREIGN KEY (poster_id) REFERENCES users(id),
+    FOREIGN KEY (username) REFERENCES users(username)
+)
+
+CREATE TABLE comments (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    FOREIGN KEY (username) REFERENCES users(username)
+)
+
+CREATE TABLE hashtags (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    FOREIGN KEY (username) REFERENCES users(username)
+)
