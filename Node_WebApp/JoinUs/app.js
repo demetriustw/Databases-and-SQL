0fb89c3fd5446725c7ff1f04ca99ff8b9ee22df6
demetriustw <56927431+demetriustw@users.noreply.github.com>
2022-12-20 19:12:03 -0700

    Add files via upload

--- Node_WebApp/JoinUs/app.js
+++ Node_WebApp/JoinUs/app.js
@@ -1,7 +1,12 @@
 const { consumers } = require("stream");
 var express = require("express");
 var mysql = require("mysql");
+var bodyParser = require("body-parser");
 var app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -10,20 +15,31 @@ var connection = mysql.createConnection({
   password: "Ago0dPas5W0rd",
 });
 
+app.post("/register", function (req, res) {
+  var person = {
+    email: req.body.email,
+  };
+
+  connection.query("INSERT INTO users SET ?", person, function (err, result) {
+    if (err) throw err;
+    res.redirect("/");
+  });
+});
+
 app.get("/", function (req, res) {
   // Find count of users in DB
-  var q = "SELECT COUNT(*) FROM users";
+  var q = "SELECT COUNT(*) AS count FROM users";
   connection.query(q, function (err, results) {
     if (err) throw err;
-    console.log(results);
+    var count = results[0].count;
+    // res.send("We have " + count + " users in our db");
+    res.render("home", { data: count });
   });
-  // Respond with that count
-  res.send("We have " + count + " users in our db");
 });
 
 app.get("/joke", function (req, res) {
   var joke =
-    "What do you call a dog that does magic tricks? A labracadabrador.";
+    "<strong>What do you call a dog that does magic tricks?</strong> <em>A labracadabrador</em>.";
   res.send(joke);
 });
 
